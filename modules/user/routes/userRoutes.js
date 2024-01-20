579c4b337a78156be49cfb2a7ebd53be1a4a7867
Piyush <piyushrajput1108@gmail.com>
2024-01-20 23:34:23 +0530

    re-configured auth api,s and added bcrypt for hashing password also added jwt for utilizing token in auth process

--- modules/user/routes/userRoutes.js
+++ modules/user/routes/userRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const userService = require("../service/userService")
 
 //routes
+router.post("/test",userService.generateTestToken);
 router.post("/sign-up",userService.registerUser);
+router.post("/sign-in",userService.login);
 
 module.exports = router
